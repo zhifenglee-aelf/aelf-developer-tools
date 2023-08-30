f7ddd0b46084edc861e51aa2f038906a0cec33d9
aelf-lu <yimeng.lu@aelf.io>
2023-08-30 18:18:41 +0800

    fix: fix bugs - update comments - update code

--- templates/HelloWorldContract/src/HelloWorldState.cs
+++ templates/HelloWorldContract/src/HelloWorldState.cs
@@ -1,13 +1,11 @@
-using System.Diagnostics.CodeAnalysis;
 using AElf.Sdk.CSharp.State;
 
 namespace AElf.Contracts.HelloWorld
 {
-    // The state class is used to communicate with the blockchain.
-    [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
+    // The state class is used to communicate with the blockchain
     public class HelloWorldState : ContractState 
     {
-        // StringState is used to put the single data.
+        // A state that holds string value
         public StringState Message { get; set; }
     }
 }
--- templates/HelloWorldContract/test/HelloWorldTests.cs
+++ templates/HelloWorldContract/test/HelloWorldTests.cs
@@ -5,7 +5,7 @@ using Xunit;
 
 namespace AElf.Contracts.HelloWorld
 {
-    // This class is unit test class, and it inherit TestBase. Write your unit test code inside it.
+    // This class is unit test class, and it inherit TestBase. Write your unit test code inside it
     public class HelloWorldTests : TestBase
     {
         [Fact]
