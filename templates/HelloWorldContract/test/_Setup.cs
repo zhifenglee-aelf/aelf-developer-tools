c58e575e5df586e565b171b17052d1fa9d249e54
aelf-lu <yimeng.lu@aelf.io>
2023-09-04 15:35:00 +0800

    fix: fix wrong comments

--- templates/HelloWorldContract/test/_Setup.cs
+++ templates/HelloWorldContract/test/_Setup.cs
@@ -3,16 +3,16 @@ using AElf.Testing.TestBase;
 
 namespace AElf.Contracts.HelloWorld
 {
-    // This class is used to load the context required for unit testing
+    // The Module class load the context required for unit testing
     public class Module : ContractTestModule<HelloWorld>
     {
         
     }
     
-    // The TestBase class inherit ContractTestBase class, which is used to define and get stub classes required for unit testing
+    // The TestBase class inherit ContractTestBase class, it defines Stub classes and gets instances required for unit testing
     public class TestBase : ContractTestBase<Module>
     {
-        // Declare stub class for unit testing
+        // The Stub class for unit testing
         internal readonly HelloWorldContainer.HelloWorldStub HelloWorldStub;
         // A key pair that can be used to interact with the contract instance
         private ECKeyPair DefaultKeyPair => Accounts[0].KeyPair;
